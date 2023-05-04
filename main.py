bf01bf0d7ff1f80e7f88134a7cbf940e24e50363
Jonathan Heer <Jon.Heer@gmail.com>
2023-05-03 20:49:55 -0500

    Removing unused username_regex. Adding language flag. Moving function into file management. Improving accuracy of experimental mode.

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
-
-import tqdm
+from tqdm import tqdm
 
 from Trys.audio_processing import load_transcription_model, detect_nonsilent_sections_from_audio, \
     create_audio_clips, transcribe_audio_clips
 from Trys.cli_args import parse_arguments
-from Trys.file_management import extract_sources_from_input_paths, load_audio, walk_src_files
-from Trys.utils import merge_transcripts, extract_username, format_timestamp
+from Trys.file_management import extract_sources_from_input_paths, load_audio, walk_src_files, export_transcript
+from Trys.utils import merge_transcripts, extract_username
 
 
 def main():
     args = parse_arguments()
 
-    input_paths, output_path, discord_usernames, experimental, min_silence_len, silence_thresh, transcription_model = (
-        args["<input>"], args['--output'], args['--discord'], args['--experimental'], int(args['--min_silence_len']),
-        int(args['--silence_thresh']), args['--transcription_model'])
+    input_paths, output_path, discord_usernames, experimental, pause_len, silence_thresh, transcription_model, language = (
+        args["<input>"], args['--output'], args['--discord'], args['--experimental'], int(args['--pause_len']),
+        int(args['--silence_thresh']), args['--transcription_model'], args['--language'])
 
     src_dir = extract_sources_from_input_paths(input_paths)
 
@@ -28,19 +27,16 @@ def main():
                 speaker = extract_username(file_name, discord_usernames)
 
                 audio = load_audio(root, file_name)
-                nonsilent_sections = detect_nonsilent_sections_from_audio(audio, file_name, min_silence_len,
-                                                                          silence_thresh)
+                nonsilent_sections = detect_nonsilent_sections_from_audio(audio, file_name, pause_len, silence_thresh)
                 audio_clips = create_audio_clips(file_name, nonsilent_sections, audio)
-                transcribed_clips = transcribe_audio_clips(audio_clips, model, speaker, min_silence_len, experimental)
+                transcribed_clips = transcribe_audio_clips(audio_clips, model, speaker, pause_len, experimental,
+                                                           language)
 
-                for (start, end), text in tqdm.tqdm(transcribed_clips, desc=f"Merging {speaker} transcript into final "
+                for (start, end), text in tqdm(transcribed_clips, desc=f"Merging {speaker} transcript into final "
                                                                             f"transcript", unit="scripts"):
                     all_transcribed_clips = merge_transcripts(all_transcribed_clips, [((start, end), speaker, text)])
 
-    with open(output_path, "w", encoding="utf-8") as f:
-        for (start, end), speaker, text in tqdm.tqdm(all_transcribed_clips, desc=f"Saving final transcript to "
-                                                                                 f"{output_path}", unit="scripts"):
-            f.write(f"{format_timestamp(start)} - {format_timestamp(end)} ({speaker}): {text}\n")
+    export_transcript(all_transcribed_clips, output_path)
 
 
 if __name__ == "__main__":
